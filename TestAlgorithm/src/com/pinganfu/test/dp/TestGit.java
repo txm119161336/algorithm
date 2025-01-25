414bd7ae0d618d04e207cffacf5cd2b324c5eb87
tanxuanming <119161336@qq.com>
2025-01-25 15:06:19 +0800

    commit system.out

--- TestAlgorithm/src/com/pinganfu/test/dp/TestGit.java
+++ TestAlgorithm/src/com/pinganfu/test/dp/TestGit.java
@@ -1,6 +1,8 @@
 package src.com.pinganfu.test.dp;
 
 public class TestGit {
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
 
 }
