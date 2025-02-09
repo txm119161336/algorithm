d7ebc967f44e1b1a46fc61230a7a4905c66b2fd2
tanxuanming <119161336@qq.com>
2025-02-09 16:21:35 +0800

    add changes new 2

--- TestAlgorithm/src/com/pinganfu/test/dp/TestGit.java
+++ TestAlgorithm/src/com/pinganfu/test/dp/TestGit.java
@@ -4,8 +4,6 @@ public class TestGit {
     public static void main(String[] args){
         System.out.println("hello world");
         System.out.println("hello world");
-        System.out.println("hello world");
-4        System.out.println("hello world");
 
     }
 
